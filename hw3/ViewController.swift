74b277f6d0db93d8314c2ae21d75b4855aa023f2
Ashton <a.turner@setonhill.edu>
2023-03-31 20:38:12 -0400

    hw 3 ready to look and grade

--- hw3/ViewController.swift
+++ hw3/ViewController.swift
@@ -8,11 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var lightOn = true
+   
+    
+    @IBOutlet weak var lightButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
+    @IBAction func buttonPressed(_ sender: Any) {
+                lightOn.toggle()
+                if lightOn{
+                    view.backgroundColor = .white
+                    lightButton.setTitle("Off", for: .normal)
+                }
+                else{
+                    view.backgroundColor = .black
+                    lightButton.setTitle("On", for: .normal)
+                }
+            }
+
+        
+        
+        
+    
 
 
 }
